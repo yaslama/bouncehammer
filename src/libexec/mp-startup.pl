e40340e09de1d0431e5b3444b4153bea9d2902c2
azumakuniyuki <azumakuniyuki@gmail.com>
2010-03-23 17:51:53 +0900

    * Perl script for mod_perl

---
+++ src/libexec/mp-startup.pl
@@ -0,0 +1,9 @@
+#__PERLEXECUTABLE__
+# $Id: mp-startup.pl,v 1.1 2010/03/23 08:51:04 ak Exp $
+use strict;
+use warnings;
+use lib '__KANADZUCHIROOT__/lib';
+use CGI::Application::Dispatch;
+
+1;
+__END__
