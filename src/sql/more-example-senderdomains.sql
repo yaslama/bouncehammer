bc9bb32f3d90e302e52ecacf55e99c59f2f68b7f
azumakuniyuki <azumakuniyuki@gmail.com>
2010-03-23 17:33:56 +0900

    * More example sender domains

---
+++ src/sql/more-example-senderdomains.sql
@@ -0,0 +1,17 @@
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.ac.jp', 'Example University');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.ad.jp', '');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.co.jp', 'Example Corporation');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.com', 'Example Inc.');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.ed.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.go.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.gr.jp', 'Example User Group Japan');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.int', 'Example International');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.lg.jp', 'Example City');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.mil', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.ne.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.net', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.or.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'exe.example.ac.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'grand.example.ed.jp', '' );
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'sp.example.jp', '');
+INSERT INTO t_senderdomains (domainname, description ) VALUES ( 'example.gl', 'Example in Greenland');
