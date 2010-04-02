e878c3d4ec0dbcd126534a4242672c417e9bb030
azumakuniyuki <azumakuniyuki@gmail.com>
2010-04-02 20:50:30 +0900

    * Fix hash structure; key name was incompatible with src/etc/*.cf

--- src/lib/Kanadzuchi/Config/TestRun.pm
+++ src/lib/Kanadzuchi/Config/TestRun.pm
@@ -1,4 +1,4 @@
-# $Id: TestRun.pm,v 1.5 2010/03/01 23:41:48 ak Exp $
+# $Id: TestRun.pm,v 1.6 2010/04/02 11:41:06 ak Exp $
 # -Id: TestRun.pm,v 1.4 2009/09/01 23:19:46 ak Exp -
 # -Id: TestRun.pm,v 1.2 2009/08/27 05:09:32 ak Exp -
 # Copyright (C) 2009,2010 Cubicroot Co. Ltd.
@@ -24,7 +24,7 @@ our $Configuration = {
 	'system'	=> 'BounceHammer',
 	'version'	=> $Kanadzuchi::VERSION,
 	'file'		=> {
-		'maxsize' => { 'cli' => 0, 'web' => 524288, },
+		'maxsize' => 0,
 		'templog' => { 'prefix' => 'hammer', 'suffix' => 'tmp' },
 		'storage' => { 'prefix' => 'hammer', 'suffix' => 'log' },
 	},
