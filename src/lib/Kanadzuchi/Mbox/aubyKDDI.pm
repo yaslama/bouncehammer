82ec74acf9c44668320bf7fdef342138d1af93e7
azumakuniyuki <azumakuniyuki@gmail.com>
2010-05-25 02:05:19 +0900

    * Support the bounce mail from auone-net.jp(mailboxfull)

--- src/lib/Kanadzuchi/Mbox/aubyKDDI.pm
+++ src/lib/Kanadzuchi/Mbox/aubyKDDI.pm
@@ -1,4 +1,4 @@
-# $Id: aubyKDDI.pm,v 1.4 2010/05/23 05:38:07 ak Exp $
+# $Id: aubyKDDI.pm,v 1.5 2010/05/24 16:54:27 ak Exp $
 # -Id: aubyKDDI.pm,v 1.1 2009/08/29 08:50:38 ak Exp -
 # -Id: aubyKDDI.pm,v 1.1 2009/07/31 09:04:51 ak Exp -
 # Kanadzuchi::Mbox::
@@ -36,13 +36,55 @@ sub detectus
 	my $phead = q();
 	my $pstat = q();
 
-	# Content-Type: text/plain; ..., X-SPASIGN: NG (spamghetti, au by KDDI)
-	# Filtered recipient returns message that include 'X-SPASIGN' header
 	if( ( $mhead->{'content-type'} =~ m{\Atext/plain} ) && ( $mhead->{'x-spasign'} eq q{NG} ) )
 	{
+		# Content-Type: text/plain; ..., X-SPASIGN: NG (spamghetti, au by KDDI)
+		# Filtered recipient returns message that include 'X-SPASIGN' header
 		$pstat  = Kanadzuchi::RFC1893->int2code(Kanadzuchi::RFC1893->internalcode('filtered'));
-		$phead .= q(Action: failed).qq(\n);
 		$phead .= q(Status: ).$pstat.qq(\n);
+	}
+	else
+	{
+		my $auone = 0;		# (Boolean) Flag, Set 1 if the line begins with the string 'Your mail sent on: ...'
+		my $error = 0;		# (Boolean) Flag, Set 1 if the line begins with the string 'Could not be delivered to..'
+		my $diagn = q();	# (String) Pseudo-Diagnostic-Code:
+		my $rxau1 = {
+			'auone' => qr{\AYour[ ]mail[ ]sent[ ]on[:][ ][A-Z][a-z]{2}[,]},
+			'error' => qr{\A\s+Could[ ]not[ ]be[ ]delivered[ ]to[:][ ]},
+		};
+		my $rxerr = {
+			'mailboxfull' => qr{\A\s+As[ ]their[ ]mailbox[ ]is[ ]full[.]\z}
+		};
+
+		# Bounced from auone-net.jp
+		EACH_LINE: foreach my $_ln ( split( qq{\n}, $$mbody ) )
+		{
+			# The line which begins with the string 'Your mail sent on: ...'
+			if( ! $auone && $_ln =~ $rxau1->{'auone'} )
+			{
+				$diagn .= $_ln;
+				$auone  = 1;
+				next();
+			}
+			elsif( ! $error && $_ln =~ $rxau1->{'error'} )
+			{
+				$diagn .= ' '.$_ln;
+				$error  = 1;
+				next();
+			}
+
+			next() if( ! $auone || ! $error );
+
+			if( $_ln =~ $rxerr->{'mailboxfull'} )
+			{
+				# Your mail sent on: Thu, 29 Apr 2010 11:04:47 +0900 
+				#     Could not be delivered to: <******@**.***.**>
+				#     As their mailbox is full.
+				$pstat  = Kanadzuchi::RFC1893->int2code(Kanadzuchi::RFC1893->internalcode('mailboxfull'));
+				$phead .= q(Status: ).$pstat.qq(\n);
+				$phead .= q(Diagnostic-Code: ).$diagn.' '.$_ln.qq(\n);
+			}
+		}
 	}
 
 	return( $phead );
