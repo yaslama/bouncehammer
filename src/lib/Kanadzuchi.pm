172b9ec86e3159abe80a7ad5f571b40e3f08cbe5
azumakuniyuki <azumakuniyuki@gmail.com>
2010-05-26 13:01:51 +0900

    * Version number is 2.3.1

--- src/lib/Kanadzuchi.pm
+++ src/lib/Kanadzuchi.pm
@@ -1,4 +1,4 @@
-# $Id: Kanadzuchi.pm,v 1.18 2010/05/16 23:59:49 ak Exp $
+# $Id: Kanadzuchi.pm,v 1.19 2010/05/26 03:57:31 ak Exp $
 # -Id: TheHammer.pm,v 1.4 2009/09/01 23:19:41 ak Exp -
 # -Id: Herculaneum.pm,v 1.13 2009/08/27 05:09:23 ak Exp -
 # -Id: Version.pm,v 1.35 2009/08/27 05:09:29 ak Exp -
@@ -47,7 +47,7 @@ __PACKAGE__->mk_accessors(
 # ||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__||
 # |/__\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|
 #
-our $VERSION = q{2.3.0};
+our $VERSION = q{2.3.1};
 our $SYSNAME = q{BounceHammer};
 our $SYSCONF = q{__KANADZUCHIROOT__/etc/bouncehammer.cf};
 
