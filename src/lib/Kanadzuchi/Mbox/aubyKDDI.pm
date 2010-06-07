592c7dd727d40af569cb744aaeb1e86d913a96ba
azumakuniyuki <azumakuniyuki@gmail.com>
2010-06-08 06:30:14 +0900

    * Add the block for the bounce message which received from MTA of ezweb.ne.jp.   Improved analytical accuracy of bin/mailboxparser(@ezweb.ne.jp)

--- src/lib/Kanadzuchi/Mbox/aubyKDDI.pm
+++ src/lib/Kanadzuchi/Mbox/aubyKDDI.pm
@@ -1,4 +1,4 @@
-# $Id: aubyKDDI.pm,v 1.5 2010/05/24 16:54:27 ak Exp $
+# $Id: aubyKDDI.pm,v 1.6 2010/06/03 06:56:45 ak Exp $
 # -Id: aubyKDDI.pm,v 1.1 2009/08/29 08:50:38 ak Exp -
 # -Id: aubyKDDI.pm,v 1.1 2009/07/31 09:04:51 ak Exp -
 # Kanadzuchi::Mbox::
@@ -42,6 +42,95 @@ sub detectus
 		# Filtered recipient returns message that include 'X-SPASIGN' header
 		$pstat  = Kanadzuchi::RFC1893->int2code(Kanadzuchi::RFC1893->internalcode('filtered'));
 		$phead .= q(Status: ).$pstat.qq(\n);
+
+	}
+	elsif( grep { $_ =~ m{\Afrom[ ]ezweb[.]ne[.]jp[ ]} } @{ $mhead->{'received'} } )
+	{
+		my $ezweb = 0;		# (Boolean) Flag, Set 1 if the line begins with the string 'The user(s) account is disabled.'
+		my $diagn = q();	# (String) Pseudo-Diagnostic-Code:
+		my $frcpt = q();	# (String) Pusedo-Final-Recipient:
+		my $icode = q();	# (String) Internal error code
+		my $etype = { 'userunknown' => 'permanent', 'suspended' => 'temporary', 'onhold' => 'permanent' };
+		my $error = { 'disable' => 0, 'limited' => 0, 'timeout' => 0 };
+		my $rxezw = {
+			'disable' => qr{\AThe[ ]user[(]s[)][ ]account[ ]is[ ]disabled[.]\z},
+			'limited' => qr{\AThe[ ]user[(]s[)][ ]account[ ]is[ ]temporarily[ ]limited[.]\z},
+			'timeout' => qr{\AThe[ ]following[ ]recipients[ ]did[ ]not[ ]receive[ ]this[ ]message:\z},
+		};
+
+		# Bounced from ezweb.ne.jp
+		EACH_LINE: foreach my $_ln ( split( qq{\n}, $$mbody ) )
+		{
+			next() if( ! $ezweb && $_ln !~ m{\AThe[ ]} );
+
+			foreach my $__e ( qw{disable limited timeout} )
+			{
+				next() unless( $_ln =~ $rxezw->{$__e} );
+
+				$diagn .= $_ln;
+				$ezweb  = 1;
+				$error->{$__e} = 1;
+				last();
+			}
+
+			next() unless( $_ln =~ m{\A[<]} );
+
+			if( $error->{'disable'} )
+			{
+				# The user(s) account is disabled.
+				if( $_ln =~ m{\A[<](.+[@].+)[>]\z} )
+				{
+					# The recipient may be unpaid user...?
+					$icode = 'suspended';
+					$frcpt = $1;
+				}
+				elsif( $_ln =~ m{\A[<](.+[@].+)[>][:]\s*.+\s*user[ ]unknown} )
+				{
+					# Unknown user
+					$icode = 'userunknown';
+					$frcpt = $1;
+				}
+			}
+			elsif( $error->{'timeout'} )
+			{
+				# THIS BLOCK IS NOT TESTED
+				# Destination host many be down ...?
+				if( $_ln =~ m{\A[<](.+[@].+)[>]\z} )
+				{
+					$icode = 'suspended';
+					$frcpt = $1;
+				}
+			}
+			elsif( $error->{'limited'} )
+			{
+				# THIS BLOCK IS NOT TESTED
+				if( $_ln =~ m{\A[<](.+[@].+)[>]\z} )
+				{
+					$icode = 'suspended';
+					$frcpt = $1;
+				}
+			}
+
+			last() if( $icode && $frcpt );
+
+		} # End of foreach(EACH_LINE)
+
+		if( $frcpt )
+		{
+			$frcpt = Kanadzuchi::RFC2822->cleanup($frcpt);
+			$icode ||= 'onhold';
+
+			if( Kanadzuchi::RFC2822->is_emailaddress($frcpt) )
+			{
+				$phead .= q(Final-Recipient: RFC822; ).$frcpt.qq(\n);
+			}
+
+			$pstat  = Kanadzuchi::RFC1893->int2code( Kanadzuchi::RFC1893->internalcode( $icode, $etype->{$icode} ) );
+			$phead .= q(Status: ).$pstat.qq(\n);
+			$phead .= q(Diagnostic-Code: ).$diagn.qq(\n);
+			$phead .= q(From: ).$mhead->{'to'}.qq(\n);
+			$phead .= q(Date: ).$mhead->{'date'}.qq(\n);
+		}
 	}
 	else
 	{
@@ -80,9 +169,10 @@ sub detectus
 				# Your mail sent on: Thu, 29 Apr 2010 11:04:47 +0900 
 				#     Could not be delivered to: <******@**.***.**>
 				#     As their mailbox is full.
-				$pstat  = Kanadzuchi::RFC1893->int2code(Kanadzuchi::RFC1893->internalcode('mailboxfull'));
+				$pstat  = Kanadzuchi::RFC1893->int2code(Kanadzuchi::RFC1893->internalcode('mailboxfull','temporary'));
 				$phead .= q(Status: ).$pstat.qq(\n);
 				$phead .= q(Diagnostic-Code: ).$diagn.' '.$_ln.qq(\n);
+				last();
 			}
 		}
 	}
