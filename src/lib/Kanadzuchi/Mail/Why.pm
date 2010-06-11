2218e5bf41d17273fd70b4f1ced90ff09bc214f1
azumakuniyuki <azumakuniyuki@gmail.com>
2010-06-11 09:45:19 +0900

    * Remove useless comment and ascii art.

--- src/lib/Kanadzuchi/Mail/Why.pm
+++ src/lib/Kanadzuchi/Mail/Why.pm
@@ -1,4 +1,4 @@
-# $Id: Why.pm,v 1.5 2010/03/01 23:41:51 ak Exp $
+# $Id: Why.pm,v 1.6 2010/06/10 10:03:13 ak Exp $
 # -Id: Pattern.pm,v 1.1 2009/08/29 07:33:13 ak Exp -
 # -Id: Pattern.pm,v 1.3 2009/05/29 08:22:25 ak Exp -
 # Copyright (C) 2009,2010 Cubicroot Co. Ltd.
@@ -12,12 +12,6 @@
  ##  ## ##  ##    ##   
                ####    
 package Kanadzuchi::Mail::Why;
-
-#  ____ ____ ____ ____ ____ ____ ____ ____ ____ 
-# ||L |||i |||b |||r |||a |||r |||i |||e |||s ||
-# ||__|||__|||__|||__|||__|||__|||__|||__|||__||
-# |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
-#
 
 #  ____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ 
 # ||G |||l |||o |||b |||a |||l |||       |||v |||a |||r |||s ||
@@ -45,10 +39,10 @@ sub is_included
 	# @Return	(Integer) 1 = includes
 	# @Return	(Integer) 0 = not
 	my $class = shift();
-	my $string = shift() || return(0);
-	my $patterns = ${"$class".'::Patterns'};
+	my $dtext = shift() || return(0);
+	my $plist = ${"$class".'::Patterns'};
 
-	foreach my $_p ( @$patterns ){ return(1) if( lc($string) =~ $_p ); }
+	foreach my $p ( @$plist ){ return(1) if( lc($dtext) =~ $p ); }
 	return(0);
 }
 
