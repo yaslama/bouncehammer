2593156797a0b9c4b68cdcbacadb8ec31a604cc0
azumakuniyuki <azumakuniyuki@gmail.com>
2010-06-11 09:41:58 +0900

    * Remove unused ascii art.

--- src/lib/Kanadzuchi/Mail/Bounced/Generic.pm
+++ src/lib/Kanadzuchi/Mail/Bounced/Generic.pm
@@ -1,4 +1,4 @@
-# $Id: Generic.pm,v 1.2 2010/02/21 20:27:31 ak Exp $
+# $Id: Generic.pm,v 1.3 2010/06/10 10:28:46 ak Exp $
 # Copyright (C) 2009,2010 Cubicroot Co. Ltd.
 # Kanadzuchi::Mail::Bounced::
                                                   
@@ -9,12 +9,6 @@
  ##  ## ##     ##  ## ##     ##       ##  ##      
   ####   ####  ##  ##  ####  ##      ####  ####   
 package Kanadzuchi::Mail::Bounced::Generic;
-
-#  ____ ____ ____ ____ ____ ____ ____ ____ ____ 
-# ||L |||i |||b |||r |||a |||r |||i |||e |||s ||
-# ||__|||__|||__|||__|||__|||__|||__|||__|||__||
-# |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
-#
 use base 'Kanadzuchi::Mail::Bounced';
 use strict;
 use warnings;
