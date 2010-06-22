f6675b7b7c8881247a2bf079468443a54755db38
azumakuniyuki <azumakuniyuki@gmail.com>
2010-06-22 22:15:12 +0900

    * Add world wide webmail service providers.

--- src/sql/mastertable-providers.sql
+++ src/sql/mastertable-providers.sql
@@ -2,3 +2,11 @@ INSERT INTO t_providers VALUES( 1, 'rfc2606', 'RFC2606 Reserved Top Level DNS Na
 INSERT INTO t_providers VALUES( 2, 'reserved', 'Other reserved Top Level DNS Names', 0 );
 INSERT INTO t_providers VALUES( 3, 'neighbor', 'Neighbor domains', 0 );
 INSERT INTO t_providers VALUES( 4, 'various', 'Various providers', 0 );
+INSERT INTO t_providers VALUES( 5, 'aol', 'AOL Mail', 0 );
+INSERT INTO t_providers VALUES( 6, 'apple', 'Apple(MobileMe)', 0 );
+INSERT INTO t_providers VALUES( 7, 'excite', 'Excite', 0 );
+INSERT INTO t_providers VALUES( 8, 'google', 'Gmail/Google Mail', 0 );
+INSERT INTO t_providers VALUES( 9, 'lycos', 'Lycos Mail', 0 );
+INSERT INTO t_providers VALUES( 10, 'microsoft', 'Windows Live/Hotmail', 0 );
+INSERT INTO t_providers VALUES( 11, 'nokia', 'Ovi by Nokia', 0 );
+INSERT INTO t_providers VALUES( 12, 'yahoo', 'Yahoo! Mail', 0 );
