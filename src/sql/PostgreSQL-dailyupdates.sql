ceb90e364a19ca1f83b9efdc78c5b8067e8d5b48
azumakuniyuki <azumakuniyuki@gmail.com>
2010-08-30 07:44:32 +0900

    * Fix syntax error.

--- src/sql/PostgreSQL-dailyupdates.sql
+++ src/sql/PostgreSQL-dailyupdates.sql
@@ -1,8 +1,8 @@
--- $Id: PostgreSQL-dailyupdates.sql,v 1.1 2010/08/16 11:57:56 ak Exp $
+-- $Id: PostgreSQL-dailyupdates.sql,v 1.2 2010/08/29 22:44:17 ak Exp $
 -- BounceHammer Daily Updates table for PostgreSQL
 
 CREATE TABLE t_dailyupdates (
-	id		INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id		INTEGER NOT NULL,
 	thetime		INTEGER NOT NULL UNIQUE,
 	thedate		CHARACTER VARYING(15) NOT NULL UNIQUE,
 	inserted	INTEGER DEFAULT 0,
@@ -14,10 +14,8 @@ CREATE TABLE t_dailyupdates (
 	description	CHARACTER VARYING(255),
 	disabled	INT2 DEFAULT 0
 );
-
 CREATE SEQUENCE t_dailyupdates_id_seq INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;
 ALTER SEQUENCE t_dailyupdates_id_seq OWNED BY t_dailyupdates.id;
 ALTER TABLE t_dailyupdates ALTER COLUMN id SET DEFAULT nextval('t_dailyupdates_id_seq'::regclass);
 ALTER TABLE ONLY t_dailyupdates ADD CONSTRAINT pk_dailyupdates PRIMARY KEY (id);
 CREATE INDEX i_dailyupdates_thedate ON t_dailyupdates USING btree (thedate);
-
