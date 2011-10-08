a1d15edbad3fceae8723416a16b92fef89800901
azumakuniyuki <azumakuniyuki@gmail.com>
2011-10-08 11:57:41 +0900

    bouncehammer 2.7.6p1

--- src/lib/Kanadzuchi.pm
+++ src/lib/Kanadzuchi.pm
@@ -1,4 +1,4 @@
-# $Id: Kanadzuchi.pm,v 1.33.2.14 2011/09/29 15:43:12 ak Exp $
+# $Id: Kanadzuchi.pm,v 1.33.2.15 2011/10/08 02:38:44 ak Exp $
 # -Id: TheHammer.pm,v 1.4 2009/09/01 23:19:41 ak Exp -
 # -Id: Herculaneum.pm,v 1.13 2009/08/27 05:09:23 ak Exp -
 # -Id: Version.pm,v 1.35 2009/08/27 05:09:29 ak Exp -
